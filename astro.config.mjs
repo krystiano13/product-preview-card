4cf6d4792d26d432ba64f9038cb432ede05d6e9c
krystiano13 <crystianotv@gmail.com>
2023-11-22 12:43:22 +0100

    fix config

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     site : 'https://krystiano13.github.io',
-    base : '/product-preview-card '
+    base : '/product-preview-card/'
 });
